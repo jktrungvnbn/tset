e0489ff4ce4d214001fd4a55523b32b73afb6bd6
jktrungvnbn <119523557+jktrungvnbn@users.noreply.github.com>
2024-05-10 17:25:16 +0700

    Delete BiA_App directory

--- BiA_App/src/BiA/Controller/Event/EventMenu.java
+++
@@ -1,8 +0,0 @@
-package BIA.Controller.Event;
-
-import java.awt.Component;
-
-//Event Menu Press
-public interface EventMenu {
-    public boolean menuPress(Component com,boolean open);
-}
--- BiA_App/src/BiA/Controller/Event/EventMenuSelected.java
+++
@@ -1,7 +0,0 @@
-
-package BIA.Controller.Event;
-
-//Event khi chọn menu (menu index + subMenu index)
-public interface EventMenuSelected {
-    public void menuSelected(int menuIndex,int subMenuIndex);
-}
